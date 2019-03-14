7465d47e6a62f7cd34e342fe6d9f5da16c281963
xgy <2406753528@qq.com>
2019-03-14 17:13:42 +0800

    td-corpus

--- tf_idf.py
+++ tf_idf.py
@@ -46,13 +46,43 @@ corpus = TextCollection(sents)
 tf_idf = []
 for sen in sents:
     td = []
-    word = []
     for data in sen:
+        elem = []
         if data not in stop_words:
-            word.append(data)
             td.append(corpus.tf_idf(data, corpus))
-    words_index = dict(zip(word, td))
-    print(words_index)
-    tf_idf.append(words_index)
+    tf_idf.append(td)
 
+# print(tf_idf[0][3])
 
+cosine = []
+for i in range(len(sents)):
+    sentences_vector = []
+    average_cosine = []
+    for w in sents[i]:
+        w = w.lower()
+        if w not in stop_words:
+            try:
+                word_index = list(words_index.keys())[list(words_index.values()).index(w)]
+                sentences_vector.append(wordVectors[word_index])
+            except ValueError:
+                continue
+    for aspect in aspect_vector:
+        sum_cosine = 0
+        for j in range(len(sentences_vector)):
+            sum_cosine = sum_cosine + \
+                         deal_data.cosine(aspect, sentences_vector[j]) * tf_idf[i][j]
+        if len(sentences_vector) != 0:
+            average_cosine.append(sum_cosine / len(sentences_vector))
+    cosine.append(average_cosine)
+
+count = 0
+count_empty = 0
+for i in range(len(sentences)):
+    if len(cosine[i]) == 0:
+        count_empty = count_empty + 1
+    else:
+        index = cosine[i].index(max(cosine[i]))
+        if aspects[index - 1] in sentences[i]['aspectCategories']:
+            count = count + 1
+print(count_empty)
+print(count / (len(sentences) - count_empty))
