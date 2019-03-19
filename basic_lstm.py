c512f86a2fadd6bc27ddb938807c8ad2dbd56eb3
xgy <2406753528@qq.com>
2019-03-19 13:50:26 +0800

    lstm2

--- basic_lstm.py
+++ basic_lstm.py
@@ -1,14 +1,22 @@
 import tensorflow as tf
 import numpy as np
 import datetime
+import deal_data
+import re
+import random
+from random import randint
+import string
+
+sentences = deal_data.restaurants_train_new()
 
 # 设置一些参数
 batchSize = 24
 units = 64
 numClasses = 2
-iterations = 100000
+iterations = 1000
 max_length = 20
 num_dimensions = 50
+aspects = ['service', 'ambience', 'anecdotes/miscellaneous', 'price', 'food']
 
 # glove词向量处理
 wordsList = np.load('data/words.npy')
@@ -40,41 +48,63 @@ prediction = (tf.matmul(last, weight) + bias)
 correctPred = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
 accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
 
-# 加载模型
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+# 建立tf会话，只有在会话中，tf才会具体计算值，包括各种占位符和变量
+sess = tf.Session()
+
+# 构造可视化图标变量
+tf.summary.scalar('Loss', loss)
+tf.summary.scalar('Accuracy', accuracy)
+merged = tf.summary.merge_all()
+logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+
+
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
 sess.run(tf.global_variables_initializer())
 
-import re
+for i in range(iterations):
+    ss = random.sample(sentences, batchSize)
 
-strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+    batchLabels = np.zeros([batchSize, 3])
+    s_s = np.zeros([batchSize, max_length])
+    s_i = 0
+    for s in ss:
+        for i in s['text']:
+            if i in string.punctuation:  # 如果字符是标点符号的话就将其替换为空格
+                s['text'] = s['text'].replace(i, " ")
+        s_w = s['text'].split()
+        s_ids = np.zeros([max_length])
+        w_i = 0
+        for w in s_w:
+            w = w.lower()
+            try:
+                s_ids[w_i] = wordsList.index(w)
+            except ValueError:
+                s_ids[w_i] = 399999  # Vector for unkown words
+            w_i += 1
+            if w_i >= 30:
+                break
+        s_s[s_i] = s_ids
 
+        bl = []
+        a_i = 0
+        sentiment = s['aspectCategories'][a] if s['aspectCategories'].__contains__(a) else 0
+        batchLabels[s_i][sentiment] = 1
+        feedDict[cv[a + '_s']] = aspect_s
+        s_i += 1
+    feedDict[labels] = batchLabels
 
-def clean_sentences(string):
-    string = string.lower().replace("<br />", " ")
-    return re.sub(strip_special_chars, "", string.lower())
+    # Write summary to Tensorboard
+    if i % 50 == 0:
+        summary = sess.run(merged, {input_data: nextBatch, labels: nextBatchLabels})
+        writer.add_summary(summary, i)
 
-
-def get_sentence_matrix(sentence):
-    sentence_matrix = np.zeros([batchSize, max_length], dtype='int32')
-    cleaned_sentence = clean_sentences(sentence)
-    split = cleaned_sentence.split()
-    for indexCounter, word in enumerate(split):
-        try:
-            sentence_matrix[0, indexCounter] = wordsList.index(word)
-        except ValueError:
-            sentence_matrix[0, indexCounter] = 399999  # Vector for unkown words
-    return sentence_matrix
-
-
-inputText = "That movie was terrible."
-inputMatrix = get_sentence_matrix(inputText)
-predictedSentiment = sess.run(prediction, {input_data: inputMatrix})[0]
-# predictedSentiment[0] represents output score for positive sentiment
-# predictedSentiment[1] represents output score for negative sentiment
-print(predictedSentiment)
-
-if predictedSentiment[0] > predictedSentiment[1]:
-    print("Positive Sentiment")
-else:
-    print("Negative Sentiment")
+    # Save the network every 10,000 training iterations
+    if i % 10000 == 0 and i != 0:
+        save_path = saver.save(sess, "models/pretrained_lstm.ckpt", global_step=i)
+        print("saved to %s" % save_path)
+writer.close()
